b7852b1389cace9bd02eec793b61343a14d71655
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-06-19 14:55:00 +0700

    added dependencies for dynamic charts

--- app/src/main/java/com/isilab/smarthome/MainActivity.kt
+++ app/src/main/java/com/isilab/smarthome/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.isilab.smarthome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var startButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+//            startActivity(Intent(this,PowerUsageActivity::class.java))
+        }
     }
 }
