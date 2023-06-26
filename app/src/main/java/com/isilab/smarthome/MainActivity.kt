bdde98689f713626d87ce73fdbef712e06091049
luongtracducanh <luongtrac.ducanh@gmail.com>
2023-06-26 15:32:26 +0700

    added Inter font and finish the Profile main screen

--- app/src/main/java/com/isilab/smarthome/MainActivity.kt
+++ app/src/main/java/com/isilab/smarthome/MainActivity.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var startButton: Button
+    private lateinit var startButton: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
